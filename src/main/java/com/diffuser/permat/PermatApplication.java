dbd7a85aa9c65c9f3ae832e4822dd18e522b5580
ryuahneee <ryuahneee@gmail.com>
2024-03-07 01:25:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/diffuser/permat/PermatApplication.java
@@ -0,0 +1,13 @@
+package com.diffuser.permat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diffuser/permat/PermatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diffuser.permat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
